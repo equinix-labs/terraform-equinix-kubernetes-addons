c67e2a4ae45beb02bf77a68b63decebd478b67f8
Óscar Cobles <68897552+ocobleseqx@users.noreply.github.com>
2022-12-02 16:21:50 +0100

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = ">= 1.11.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+  }
+}
