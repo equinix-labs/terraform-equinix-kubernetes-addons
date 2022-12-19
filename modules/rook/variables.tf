b4c38fe20b2dbfa0dde22c8fb11f0055b79ad7f9
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2022-12-19 13:43:13 -0600

    Removed unused variables.
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- modules/rook/variables.tf
+++ modules/rook/variables.tf
@@ -7,32 +7,3 @@
 # TEMPLATE: https://www.terraform.io/docs/language/values/variables.html
 # TEMPLATE: https://www.terraform.io/docs/language/expressions/types.html
 #
-
-variable "ssh_config" {
-  description = "Connection details to apply configuration"
-  type = object({
-    host        = string
-    user        = optional(string)
-    private_key = string
-  })
-}
-
-variable "addon_context" {
-  description = "Input context for the addon"
-  type = object({
-    equinix_project        = string
-    equinix_metro          = string
-    kubeconfig_remote_path = string
-  })
-}
-
-variable "addon_config" {
-  description = "Add-on configuration for Longhorn"
-  type        = any
-  //TODO should we define all options here? 
-  # type = object({
-  #   lb_services_name = list(string)
-  #   lb_bgp_customer_asn = number
-  # })
-  default = {}
-}
