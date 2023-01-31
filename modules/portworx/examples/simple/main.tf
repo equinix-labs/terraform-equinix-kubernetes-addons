48e2b23a44aa8be6154f82455745c2ae8e962b36
Keerthivasan S <ksuresh@purestorage.com>
2023-01-31 08:48:52 +0000

    Modified kubeconfig variable

--- modules/portworx/examples/simple/main.tf
+++ modules/portworx/examples/simple/main.tf
@@ -13,13 +13,7 @@ provider "equinix" {
 module "equinix_kubernetes_addons" {
   source = "../../../../"
 
-  equinix_project = var.metal_project_id
-  equinix_metro   = "SV"
-
-  ssh_host        = var.host
-  ssh_private_key = file(var.private_key_path)
-
-  kubeconfig_remote_path = var.kubeconfig_remote_path
+  kubeconfig_local_path = var.kubeconfig_local_path
 
   enable_portworx = true
 
